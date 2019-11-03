33f4e4a0a0cf807b60d34cb2832d3828af7a04ab
Jay <55824029+skill9b@users.noreply.github.com>
2019-11-03 18:59:48 +1300

    Layout for graphs

--- GD1M02Project/resource.h
+++ GD1M02Project/resource.h
@@ -7,9 +7,32 @@
 #define IDR_MENU                        103
 #define ID_BFSDFS                       104
 #define ID_ASTAR                        105
-#define BFSDFS_BUTTON					1001
-#define ASTAR_BUTTON					1002
 #define ID_EXIT                         106
+#define BFSDFS_BUTTON                   1001
+#define IDC_EDIT1                       1001
+#define ASTAR_BUTTON                    1002
+#define IDC_EDIT2                       1002
+#define IDC_EDIT3                       1003
+#define IDC_EDIT4                       1004
+#define IDC_EDIT5                       1005
+#define IDC_EDIT6                       1006
+#define IDC_EDIT7                       1007
+#define IDC_EDIT8                       1008
+#define IDC_EDIT9                       1009
+#define IDC_EDIT10                      1010
+#define IDC_EDIT11                      1011
+#define IDC_EDIT12                      1012
+#define IDC_EDIT13                      1013
+#define IDC_EDIT14                      1014
+#define IDC_EDIT15                      1015
+#define IDC_EDIT16                      1016
+#define IDC_EDIT17                      1017
+#define IDC_EDIT18                      1018
+#define IDC_EDIT19                      1019
+#define IDC_EDIT20                      1020
+#define IDC_EDIT21                      1021
+#define IDC_EDIT22                      1022
+#define IDC_BUTTON1                     1023
 
 // Next default values for new objects
 // 
@@ -17,7 +40,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         40003
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1024
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
